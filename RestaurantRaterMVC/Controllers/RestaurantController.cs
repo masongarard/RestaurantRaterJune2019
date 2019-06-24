6fef69a2f34cdd54f7a7585fbbd0b06529765cfa
masongarard <masongarard@gmail.com>
2019-06-24 16:57:24 -0400

    Final Full CRUD

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -40,6 +40,25 @@ namespace RestaurantRaterMVC.Controllers
             return View(restaurant);
         }
 
+        //GET: RestauraNT/Details/{id}
+
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(restaurant);
+        }
+
+
         //GET: Restaurant/Edit/{id}
         public ActionResult Edit(int? id)
         {
